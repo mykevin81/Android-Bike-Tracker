facad01f8120e1e1fd448cf1315e1eb20f70cb07
mykevin81 <mykevin81@gmail.com>
2015-04-06 03:09:59 -0700

    Included Function that activates Start button *Added a Custom Class called PauseableChronometer, need to implement into the activity *Added various button icons *Make screen always on in TrackerActivity.

--- app/src/main/java/com/mykevin81/kevin/biketracker/MainActivity.java
+++ app/src/main/java/com/mykevin81/kevin/biketracker/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
+
                 startActivity(new Intent(c, TrackerActivity.class));
                 finish();
 
--- app/src/main/java/com/mykevin81/kevin/biketracker/TrackerActivity.java
+++ app/src/main/java/com/mykevin81/kevin/biketracker/TrackerActivity.java
@@ -3,21 +3,52 @@ package com.mykevin81.kevin.biketracker;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Chronometer;
 
 
 
-public class TrackerActivity extends Activity {
+public class TrackerActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
 
-        //always show the current time in this box
-        final Chronometer Current_Time = (Chronometer) findViewById(R.id.timer);
-        Current_Time.setBase(SystemClock.elapsedRealtime());
-        Current_Time.start();
+        final Button start_pause = (Button) findViewById(R.id.start_pause_btn);
+        final String Pause_String = getResources().getString(R.string.Pause_Button);
+
+        start_pause.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+
+                Chronometer Current_Time = (Chronometer) findViewById(R.id.timer);
+                Current_Time.setBase(SystemClock.elapsedRealtime());
+
+                if (start_pause.getText() != Pause_String) {
+
+                    //change Start button text to pause
+                    start_pause.setText(Pause_String);
+
+                    //Start the timer
+
+                    Current_Time.start();
+
+                } else {
+                    start_pause.setText(R.string.Resume_Button);
+                    Current_Time.stop();
+                }
+
+            }
+
+
+        });
+
 
 
     }
