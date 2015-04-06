ea762db13d48dc09601b33231563d129da29e236
mykevin81 <mykevin81@gmail.com>
2015-04-05 22:05:58 -0700

    Added Chromometer that should be changed to timer, Added Google Maps fragment

--- app/src/main/java/com/mykevin81/kevin/biketracker/TrackerActivity.java
+++ app/src/main/java/com/mykevin81/kevin/biketracker/TrackerActivity.java
@@ -2,6 +2,9 @@ package com.mykevin81.kevin.biketracker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.widget.Chronometer;
+
 
 
 public class TrackerActivity extends Activity {
@@ -10,4 +13,22 @@ public class TrackerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
-    }}
+
+        //always show the current time in this box
+        final Chronometer Current_Time = (Chronometer) findViewById(R.id.time);
+        Current_Time.setBase(SystemClock.elapsedRealtime());
+        Current_Time.start();
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
