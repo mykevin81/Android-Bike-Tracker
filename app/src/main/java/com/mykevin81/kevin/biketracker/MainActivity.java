5510e794398ddb25500023a07ad4434486516a4e
mykevin81 <mykevin81@gmail.com>
2015-04-06 00:21:41 -0700

    Finished basic layout for tracker activity

--- app/src/main/java/com/mykevin81/kevin/biketracker/MainActivity.java
+++ app/src/main/java/com/mykevin81/kevin/biketracker/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         Button start = (Button)findViewById(R.id.Start_button);
-    /**Enter tracking activity when Start button from Welcome page is pressed */
+        /**Enter tracking activity when Start button from Welcome page is pressed */
 
 
         final Context c = this;
@@ -29,11 +29,5 @@ public class MainActivity extends Activity {
                 // TODO Auto-generated method stub
                 startActivity(new Intent(c, TrackerActivity.class));
                 finish();
-  /**
-   * NOTE FROM crua9
-   * Just compare this page to others when you want to remove the actionbar stuff. I moved the detection of the button within the on create, and allowed it to find the class quicker.
-   * Check the manifest.
-   *
-   * BTW, I didn't know if you wanted to keep this class open. If you do, then take off the finish(). This basically kills this class so it doesn't eat up the users memory and battery.
-   */
+
             }});}}
