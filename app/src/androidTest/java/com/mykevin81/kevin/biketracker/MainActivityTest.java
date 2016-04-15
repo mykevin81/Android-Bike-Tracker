1299b901bc86bfac4fc536ce52eb32f417ca4eda
mykevin81 <mykevin81@gmail.com>
2016-04-15 13:41:57 -0700

    (ADD) Start button test

--- app/src/androidTest/java/com/mykevin81/kevin/biketracker/MainActivityTest.java
+++ app/src/androidTest/java/com/mykevin81/kevin/biketracker/MainActivityTest.java
@@ -5,6 +5,8 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 
@@ -21,5 +23,10 @@ public class MainActivityTest {
     @Test
     public void shouldDisplayWelcomeText() {
         onView(withText("Welcome Biker!")).check(ViewAssertions.matches(isDisplayed()));
+    }
+
+    @Test
+    public void startButtonTest() {
+        onView(withId(R.id.Start_button)).check(ViewAssertions.matches(isClickable()));
     }
 }
