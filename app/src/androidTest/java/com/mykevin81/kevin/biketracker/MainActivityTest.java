53b6950ca250981167423d31a8a44b4501b99d27
mykevin81 <mykevin81@gmail.com>
2016-04-15 03:12:59 -0700

    (ADD) Test MainActivity with welcome text "Welcome Biker!"

--- app/src/androidTest/java/com/mykevin81/kevin/biketracker/MainActivityTest.java
+++ app/src/androidTest/java/com/mykevin81/kevin/biketracker/MainActivityTest.java
@@ -1,4 +1,25 @@
 package com.mykevin81.kevin.biketracker;
 
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+
+    @Rule
+    public final ActivityTestRule<MainActivity> mainActivity = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void shouldDisplayWelcomeText() {
+        onView(withText("Welcome Biker!")).check(ViewAssertions.matches(isDisplayed()));
+    }
 }
