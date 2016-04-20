dc4cf0cf7e743991c8cf849894b307a0abaf1890
mykevin81 <mykevin81@gmail.com>
2016-04-20 14:26:25 -0700

    (ADD)click test for start pause button and stop button

--- app/src/androidTest/java/com/mykevin81/kevin/biketracker/TrackerActivityTest.java
+++ app/src/androidTest/java/com/mykevin81/kevin/biketracker/TrackerActivityTest.java
@@ -1,16 +1,34 @@
 package com.mykevin81.kevin.biketracker;
 
+import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class TrackerActivityTest {
 
     @Rule
     public final ActivityTestRule<TrackerActivity> trackerActivity = new ActivityTestRule<>(TrackerActivity.class);
+
+
+    @Test
+    public void startPauseButtonClickTest() {
+        onView(withId(R.id.start_pause_btn)).check(ViewAssertions.matches(isClickable()));
+    }
+
+    @Test
+    public void stopButtonClickTest() {
+        onView(withId(R.id.Stop_btn)).check(ViewAssertions.matches(isClickable()));
+    }
+
 
 
 }
