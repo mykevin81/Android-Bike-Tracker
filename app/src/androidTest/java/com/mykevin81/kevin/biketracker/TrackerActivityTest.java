6134f402ae1de939af849a2c2a03ff85a98b480f
mykevin81 <mykevin81@gmail.com>
2016-04-20 22:19:53 -0700

    (ADD)add some test

--- app/src/androidTest/java/com/mykevin81/kevin/biketracker/TrackerActivityTest.java
+++ app/src/androidTest/java/com/mykevin81/kevin/biketracker/TrackerActivityTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
@@ -29,6 +30,30 @@ public class TrackerActivityTest {
         onView(withId(R.id.Stop_btn)).check(ViewAssertions.matches(isClickable()));
     }
 
+    @Test
+    public void settingButtonClickTest() {
+        onView(withId(R.id.SettingButton)).check(ViewAssertions.matches(isClickable()));
+    }
+
+    @Test
+    public void clockViewExistsTest() {
+        onView(withId(R.id.clock)).check(ViewAssertions.matches(isDisplayed()));
+    }
+
+    @Test
+    public void timerViewExistsTest() {
+        onView(withId(R.id.timer)).check(ViewAssertions.matches(isDisplayed()));
+    }
+
+    @Test
+    public void speedViewExistsTest() {
+        onView(withId(R.id.tv_speed)).check(ViewAssertions.matches(isDisplayed()));
+    }
+
+    @Test
+    public void cadenceViewExistsTest() {
+        onView(withId(R.id.tv_cadence)).check(ViewAssertions.matches(isDisplayed()));
+    }
 
 
 }
